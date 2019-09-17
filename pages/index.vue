32e12060d72cd4be044f6a7bec1f6d2d52d03069
Dries Bos <info@driesbos.com>
2019-09-17 10:55:17 +0300

    error catch

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,10 @@
     <div class="center-block">
       <input v-model="input" @keyup.enter="submitQuery(input)" placeholder="input" type="text" />
       <button @click="submitQuery(input)">submit</button>
-      <p>{{ info }}</p>
+      <p v-if="info">{{ info }}</p>
+      <p
+        v-if="errored"
+      >We're sorry, we're not able to retrieve this information at the moment, please try back later</p>
     </div>
   </div>
 </template>
@@ -16,7 +19,8 @@ export default {
   data() {
     return {
       info: null,
-      input: ""
+      input: "",
+      errored: false
     };
   },
   methods: {
@@ -24,11 +28,12 @@ export default {
       if (input !== "") {
         axios
           .get(
-            `${aviationEdgeUri}flights?key=${aviationEdgeKey}&flightIata=${input}`
+            `${aviationEdgeUri}flights?key=${aviationEdgeKey}&limit=1&flightIata=${input}`
           )
-          .then(response => (this.info = response))
+          .then(response => (this.info = response.data))
           .catch(error => {
-            console.log(error.response);
+            console.log(error);
+            this.errored = true;
           });
         this.input = "";
       }
