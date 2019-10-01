02b804cf6b921039439fe2e10fc698c549f81746
Dries Bos <info@driesbos.com>
2019-10-01 15:01:09 +0400

    added:  calculation by dep/arr points

--- pages/index.vue
+++ pages/index.vue
@@ -2,45 +2,55 @@
   <div class="container">
     <div class="content-block">
       <transition name="content-change" mode="out-in">
-        <!------------ Input ------------>
-        <div v-if="!result" class="input-block" key="input">
-          <input
-            id="flightNumInput"
-            v-model="input"
-            @keyup.enter="submitQuery(input)"
-            placeholder="flightnumber"
-            type="text"
-          />
-          <div class="select-container">
-            <select v-model="seat" @change="seatCalculation(seat, carbonTotal)">
-              <option value="economy">economy</option>
-              <option value="business">business</option>
-            </select>
-            <span>&nbsp;seat</span>
+        <!------------ Inputs ------------>
+        <div v-if="!result" class="input-container" key="input">
+          <div class="input-block">
+            <input
+              id="flightNumInput"
+              v-model="input"
+              @keyup.enter="submitFlightQuery(input)"
+              placeholder="flightnumber"
+              type="text"
+            />
+            <div class="select-container">
+              <select v-model="seat" @change="seatCalculation(seat, carbonTotal)">
+                <option value="economy">economy</option>
+                <option value="business">business</option>
+              </select>
+              <span>&nbsp;seat</span>
+            </div>
+            <button @click="submitFlightQuery(input)" class="reset-button">submit</button>
           </div>
-          <button @click="submitQuery(input)" class="reset-button">submit</button>
+
+          <div class="input-block">
+            <input
+              id="departureInput"
+              v-model="departureResult"
+              @keyup.enter="submitAiportQuery(departureResult, arrivalResult)"
+              placeholder="departure"
+              type="text"
+            />
+            <input
+              id="arrivalInput"
+              v-model="arrivalResult"
+              @keyup.enter="submitAirportQuery(departureResult, arrivalResult)"
+              placeholder="arrival"
+              type="text"
+            />
+            <button
+              @click="submitAirportQuery(departureResult, arrivalResult)"
+              class="reset-button"
+            >submit</button>
+          </div>
         </div>
         <!------------ Output ------------>
+
         <div v-if="result" class="output-block" key="output">
           <ul>
-            <li>
-              <span>Airline</span>
-              {{ result.airline.iataCode }}
-            </li>
-            <li>
-              <span>Aircraft</span>
-              {{ result.aircraft.iataCode }}
-            </li>
-            <li>
-              <span>From</span>
-              {{ result.departure.iataCode }}
-              <span>to</span>
-              {{ result.arrival.iataCode }}
-            </li>
-            <li>
-              {{ seat }}
-              <span>seat</span>
-            </li>
+            <!-- <li v-if="distanceResult">
+              <span>Distance</span>
+              {{ distanceResult }}
+            </li>-->
             <li>
               <span>Carbon footprint:</span>
               {{ carbonTotal }}
@@ -62,15 +72,22 @@
 
 <script>
 // TODO
-// Store output departure/arrival Iata in constants
-// Use departure/arrival constants to calculate distance through airline routes API
-// Create and show distance constant
-// TODO
-// Add function to change aircraftIata to aircraftType
-// TOFIX
+// 1 = Learn how to pass data as arguments into methods functions and when 'this.' is needed and how to output the data
+// 2 = use the submitAirportQuery WITHIN the submitFlightQuery to calculate carbonTotal
+// 3 = UX -> toggle between airport/flightnr input + animate carbonTotal result with AnimeJS
+// TODO LATER
+// Add averages (EU first + later get visitor country data)
+// Use aircarrier + flightheight in calculations
 // if loading + if error
+// Approach scientist for proper calculations
 import axios from "axios";
-import { aviationEdgeKey, aviationEdgeUri, proxy } from "~/plugins/config";
+import {
+  aviationEdgeKey,
+  aviationEdgeUri,
+  greatCircleMapperKey,
+  greatCircleMapperUri,
+  proxy
+} from "~/plugins/config";
 
 export default {
   data() {
@@ -80,8 +97,9 @@ export default {
       departureResult: "",
       arrivalResult: "",
       aircraftResult: "",
-      // errored: false,
-      // loading: false,
+      distanceResult: "",
+      errored: false,
+      loading: false,
       // altitudeOffset: 0,
       seat: "economy",
       carbonSeatMultiplier: false,
@@ -93,7 +111,8 @@ export default {
     inputFocus: function() {
       document.getElementById("flightNumInput").focus();
     },
-    submitQuery: function(input) {
+
+    submitFlightQuery: function(input) {
       let inputTrimmed = input.trim() && input.replace(/ +/g, "");
       if (inputTrimmed !== "") {
         axios
@@ -102,14 +121,14 @@ export default {
           )
           .then(response => {
             this.result = response.data[0];
-            this.departureResult = response.data[0].departure.iataCode;
-            this.arrivalResult = response.data[0].arrival.iataCode;
+            this.departureResult = response.data[0].departure.icaoCode;
+            this.arrivalResult = response.data[0].arrival.icaoCode;
             this.aircraftResult = response.data[0].aircraft.iataCode;
             console.log("RESULT", this.result);
           })
           .catch(error => {
-            this.errored = true;
-            console.log("ERROR", response.error);
+            // this.errored = true;
+            console.log("submitFlightQuery ERROR", response.error);
           });
         this.input = "";
       } else {
@@ -118,11 +137,35 @@ export default {
         document.getElementById("flightNumInput").focus();
       }
     },
+
     seatCalculation: function(seat, carbonTotal) {
       if (seat === "business") {
         this.carbonTotal = this.carbonTotal * 2;
       }
     },
+
+    submitAirportQuery: function(departureResult, arrivalResult, carbonTotal) {
+      axios({
+        method: "GET",
+        url: `${greatCircleMapperUri}airports/route/${departureResult}-${arrivalResult}/510`,
+        headers: {
+          "x-rapidapi-host": "greatcirclemapper.p.rapidapi.com",
+          "x-rapidapi-key": `${greatCircleMapperKey}`,
+          vary: "Accept-Encoding"
+        }
+      })
+        .then(response => {
+          this.result = response.data.totals.distance_km;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      carbonTotal = this.result * 0.3;
+      console.log(this.result);
+      this.departureResult = "";
+      this.arrivalResult = "";
+    },
+
     // aircraftType: function(result) {
     //   const urlPath = "greatcirclemapper.p.rapidapi.com";
     //   const aircraftCode = result.aircraft.iataCode;
@@ -137,6 +180,7 @@ export default {
     //       console.log(error);
     //     });
     // },
+
     resetResults: function(result) {
       this.result = "";
       console.log(this.result);
