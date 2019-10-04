317cf09a36f62e1f0f976ce1889925960ebc98a2
Dries Bos <info@driesbos.com>
2019-10-04 14:16:50 +0400

    removed: offset content section

--- pages/index.vue
+++ pages/index.vue
@@ -163,23 +163,6 @@
           v-if="errored"
         >We're sorry, we're not able to retrieve this resultrmation at the moment, please try back later</p>
       </div>
-      <!------------ Output ------------>
-      <div v-if="showOffset" class="offset-block" key="offset">
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-        <div @click="resetResults" class="bar-toggle" title="reset values">
-          <svg class="reset">
-            <path
-              d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
-            />
-            <path d="M0 0h24v24H0z" fill="none" />
-          </svg>
-        </div>
-      </div>
     </div>
   </div>
 </template>
